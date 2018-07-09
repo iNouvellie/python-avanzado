601f1aa9be96dd41146b29af47fc0af740264548
Roberto Rocuant <r.rocuant4@hotmail.com>
2018-07-09 19:58:16 -0400

    Creación
    Creación de modulos y demostración de import de los mismos

---
+++ 02_Modulos/01_Creacion.py
@@ -0,0 +1,8 @@
+#Importo los archivos externos modulo y moduloZ
+#Le creo un alias al modulo para no reescribir su nombre
+import modulo as A
+import moduloZ as B
+
+#Llamo los metodos de los modulos externos
+A.metodo()
+B.metodo()
