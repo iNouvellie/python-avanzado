dfda17d82770b4eb95ce738054127a3f194deec1
Roberto Rocuant <r.rocuant4@hotmail.com>
2018-07-09 20:03:45 -0400

    Paquetes
    Importar paquetes en directorios externos y llamar sus metodos

---
+++ 02_Modulos/03_Paquetes.py
@@ -0,0 +1,8 @@
+#Importamos el moduloA y B de la carpeta paquetes
+from paquetes import moduloA
+from paquetes import moduloB
+
+#Llamamos el metodoX y metodoY
+moduloA.metodoX()
+
+moduloB.metodoY()
