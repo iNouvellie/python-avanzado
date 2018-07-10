5e8521c99e09ba2f015d26c463b11e8341daa175
Roberto Rocuant <r.rocuant4@hotmail.com>
2018-07-10 00:36:36 -0400

    Borrar dato
    Se borra datos de la tabla example creada anteriormente

---
+++ 04_MySQL/03_BorrarDato.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+con = mysql.connector.connect(user = "root", password = "", host = "127.0.0.1", database = "bdpython")
+
+cursor = con.cursor()
+
+cursor.execute("DELETE FROM 'example' WHERE 'id' = 9;")
+
+con.commit()
+cursor.close()
+con.close()
