8b1933295dcded2e8b45b903b9d4961c42d27d2d
Roberto Rocuant <r.rocuant4@hotmail.com>
2018-07-09 23:26:07 -0400

    Insertar datos
    Aqui aprendemos a crear la tabla y a insertar datos desde Python

--- 04_MySQL/01_InsertarDatos.py
+++ 04_MySQL/01_InsertarDatos.py
@@ -1,8 +1,16 @@
 import mysql.connector
 
-con = mysql.connector.connect(user = "root", password = "", host = "127.0.0.1", database = "bdpythpn")
+con = mysql.connector.connect(user = "root", password = "", host = "127.0.0.1", database = "bdpython")
 
 cursor = con.cursor()
-cursor.execute("CREATE TABLE example (id INT, data VARCHAR(100));")
+
+#Crea la tabla (lo comente desp de crearla)
+#cursor.execute("CREATE TABLE example (id INT, data VARCHAR(100));")
+
+#Con esto insertamos datos en la tabla
+cursor.execute("INSERT INTO example (id, data) VALUES ('9', 'dato')")
+
+#Con esto se almacenan los datos creados (commit)
+con.commit()
 
 con.close()
