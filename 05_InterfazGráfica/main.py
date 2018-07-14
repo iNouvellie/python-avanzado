ba20d2b7ad4e55fbd163d7c445841bce736de540
Roberto Rocuant <r.rocuant4@hotmail.com>
2018-07-14 15:44:11 -0400

    Archivo borrado
    Archivo borrado y ejemplificado en otro

--- 05_InterfazGráfica/main.py
+++
@@ -1,9 +0,0 @@
-import wx
-
-#Creacion de un objeto
-app = wx.App()
-#None, elemento padre, -1 asigna un ID al elemento automaticamente, texto es un titulo
-frame = wx.Frame(None, -1, 'Primer Ventana')
-frame.Show()
-
-app.MainLoop()
